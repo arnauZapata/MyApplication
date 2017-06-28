bf880d69955b5a92890964ae540715e042c27927
arnauZapata <arnauzapata@hotmail.com>
2017-06-28 18:04:46 +0200

    Ahora mi aplicacion sabe memorizar datos. Ahora tiene mas memoria que yo

--- app/src/main/java/com/arnauzapata/myapplication/Bandera.java
+++ app/src/main/java/com/arnauzapata/myapplication/Bandera.java
@@ -1,13 +1,17 @@
 package com.arnauzapata.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Bandera extends AppCompatActivity {
-
+    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bandera);
+       // Intent i = new Intent(context,Bandera.class);
+       // startActivity(i);
     }
 }
--- app/src/main/java/com/arnauzapata/myapplication/MainActivity.java
+++ app/src/main/java/com/arnauzapata/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.arnauzapata.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -19,46 +21,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button buttonOne, buttonTwo, buttonTree;
     TextView textOne;
     Toolbar toolbar;
+    EditText editText;
 
     private void setText(String text){
         textOne.setText(text);
 
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.v(TAG,"onCreate");
-        numero = 1;
-        //inicializacion
+    private void inicializarObjetos() {
         buttonOne = (Button) findViewById(R.id.button1);
         buttonTwo = (Button) findViewById(R.id.button2);
         buttonTree = (Button) findViewById(R.id.button3);
         textOne = (TextView) findViewById(R.id.text1);
         toolbar = (Toolbar) findViewById(R.id.toolBar1);
-        toolbar.setTitle("TOOLBAR");
-        setSupportActionBar(toolbar);
+    }
+
+    private void asignarOnClickListener() {
+        final Context context = this;
         buttonOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.v(TAG, "Soy el boton 1");
                 setText("Soy el boton 1");
+                Intent i = new Intent(context,Bandera.class);
+                startActivity(i);
             }
         });
         buttonTwo.setOnClickListener(this);
         buttonTree.setOnClickListener(this);
-        setText("Aun no me han escrito nada");
-        toolbar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        Intent i = new Intent(this,Bandera.class);
-        startActivity(i);
-
         /*View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,6 +57,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         };
         buttonOne.setOnClickListener(listener);*/
+    }
+
+    private void asignarValoresObjetos() {
+        toolbar.setTitle("TOOLBAR");
+        setSupportActionBar(toolbar);
+        setText("Aun no me han escrito nada");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v(TAG,"onCreate");
+        numero = 1;
+        inicializarObjetos();
+        asignarValoresObjetos();
+        asignarOnClickListener();
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState( savedInstanceState);
+        Log.v(TAG,"se ha llamado onRestore");
+        String aux = savedInstanceState.getString("text");
+        setText(aux);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString("text", (String) textOne.getText());
+        Log.v(TAG,"se ha llamado onSaveInstanceState");
 
     }
 
@@ -107,6 +127,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void meHanHechoClick(View v){
         Log.v(TAG, "Soy el boton 4");
         setText("Soy el boton 4");
+       // int a= Integer.parseInt(editText.getText().toString());
+        //int b= Integer.parseInt(editText.getText().toString());
+        //textOne.setText(String.valueOf(a+b));
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
