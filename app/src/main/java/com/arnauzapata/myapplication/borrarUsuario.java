b769bf0c1296ef319880601ca50420a14438b778
arnauZapata <arnauzapata@hotmail.com>
2017-07-05 02:14:34 +0200

    Login: Funcional;  ifunciones en fase de pruebas

--- app/src/main/java/com/arnauzapata/myapplication/borrarUsuario.java
+++ app/src/main/java/com/arnauzapata/myapplication/borrarUsuario.java
@@ -4,9 +4,15 @@ package com.arnauzapata.myapplication;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -17,7 +23,11 @@ import android.view.ViewGroup;
 public class borrarUsuario extends Fragment {
 
     private static Context context;
-
+    private MyDataBaseHelper1 myDataBaseHelper;
+    private EditText editTextUsuari;
+    private EditText editTextOldContrasenya;
+    private Button buttonAceptar;
+    private Button  buttonAtras;
 
     public borrarUsuario() {
         // Required empty public constructor
@@ -38,8 +48,39 @@ public class borrarUsuario extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_borrar_usuario, container, false);
+        View view = inflater.inflate(R.layout.fragment_borrar_usuario, container, false);
+        editTextUsuari = (EditText) view.findViewById(R.id.editTextCambiarContrasenyaUsuario);
+        editTextOldContrasenya = (EditText) view.findViewById(R.id.editTextCambiarContrasenyaOldPassword);
+        buttonAceptar = (Button) view.findViewById(R.id.buttonCambiarContrasenyaAceptar);
+        buttonAtras = (Button) view.findViewById(R.id.buttonAtras);
+        myDataBaseHelper = new MyDataBaseHelper1(context);
+        myDataBaseHelper.getInstance(context);
+        buttonAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editTextUsuari.getText().toString();
+                String s1 = editTextOldContrasenya.getText().toString();
+                String id = myDataBaseHelper.queryRow(s);
+                if(s1.equals(id)){
+                    Log.v(TAG,"va todo correcto")  ;
+                    myDataBaseHelper.deleteRow(s);
+                }
+                else Log.v(TAG,"va todo INcorrecto") ;
+            }
+        });
+        buttonAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction trans = getFragmentManager().beginTransaction();
+                LoginOtros fragment = new LoginOtros();
+                fragment.newInstance(context);
+                trans.replace(R.id.fragment_login_container,fragment);
+                trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                trans.addToBackStack(null);
+                trans.commit();
+            }
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/arnauzapata/myapplication/cambiarContrasenya.java
+++ app/src/main/java/com/arnauzapata/myapplication/cambiarContrasenya.java
@@ -4,6 +4,8 @@ package com.arnauzapata.myapplication;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +21,13 @@ import android.widget.EditText;
 public class cambiarContrasenya extends Fragment {
 
     private static Context context;
+    private EditText editTextUsuari;
+    private EditText editTextOldContrasenya;
+    private EditText editTextNewContrasenya;
+    private Button buttonAceptar;
+    private Button  buttonAtras;
+    private MyDataBaseHelper1 myDataBaseHelper;
+    private String TAG="cambiarContrasenya";
 
 
     public cambiarContrasenya() {
@@ -41,10 +50,40 @@ public class cambiarContrasenya extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cambiar_contrasenya, container, false);
         editTextUsuari = (EditText) view.findViewById(R.id.editTextCambiarContrasenyaUsuario);
-        editTextOldContrasenya = (EditText) view.findViewById(R.id.editTextLoginPass);
-        editTextNewContrasenya = (EditText) view.findViewById(R.id.editTextLoginPass);
-        buttonAceptar = (Button) view.findViewById(R.id.buttonSignIn);
-        buttonAtras = (Button) view.findViewById(R.id.buttonLogIn);
+        editTextOldContrasenya = (EditText) view.findViewById(R.id.editTextCambiarContrasenyaOldPassword);
+        editTextNewContrasenya = (EditText) view.findViewById(R.id.editTextCambiarContrasenyaNewPassword);
+        buttonAceptar = (Button) view.findViewById(R.id.buttonCambiarContrasenyaAceptar);
+        buttonAtras = (Button) view.findViewById(R.id.buttonAtras);
+        myDataBaseHelper = new MyDataBaseHelper1(context);
+        myDataBaseHelper.getInstance(context);
+        buttonAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = editTextUsuari.getText().toString();
+                String oldPassW = editTextOldContrasenya.getText().toString();
+                String newPassW = editTextNewContrasenya.getText().toString();
+                String id = myDataBaseHelper.queryRow(user);
+                if(oldPassW.equals(id)){
+                    myDataBaseHelper.updateRow(user,newPassW);
+                    Log.v(TAG,"va todo correcto")  ;                  //HACE ALGO
+                }
+                else Log.v(TAG,"va todo INcorrecto") ;
+                //.makeText(view.getContext(),"We created a row with "+id+"!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction trans = getFragmentManager().beginTransaction();
+                LoginOtros fragment = new LoginOtros();
+                fragment.newInstance(context);
+                trans.replace(R.id.fragment_login_container,fragment);
+                trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                trans.addToBackStack(null);
+                trans.commit();
+                //.makeText(view.getContext(),"We created a row with "+id+"!", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
