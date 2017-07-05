f6bdfb7bc6cc37d76e2f2ba6809c590b9faba453
arnauZapata <arnauzapata@hotmail.com>
2017-07-05 17:15:33 +0200

    Memory: iMPLEMENTANDO, falta generar solucion, enlazar con solucion, esperar y volver al fragment inicial.

---
+++ app/src/main/java/com/arnauzapata/myapplication/jocMemory.java
@@ -0,0 +1,157 @@
+package com.arnauzapata.myapplication;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.material.joanbarroso.flipper.CoolImageFlipper;
+
+
+public class jocMemory extends android.app.Fragment implements View.OnClickListener {
+
+    private static final String TAG = "Joc Memory";
+    private OnFragmentInteractionListener mListener;
+    private ImageView ImageView1;
+    private ImageView ImageView2;
+    private ImageView ImageView3;
+    private ImageView ImageView4;
+    private ImageView ImageView5;
+    private ImageView ImageView6;
+    private ImageView ImageView7;
+    private ImageView ImageView8;
+    private ImageView ImageView9;
+    private ImageView ImageView10;
+    private ImageView ImageView11;
+    private ImageView ImageView12;
+    Context context;
+
+    public jocMemory() {
+        // Required empty public constructor
+    }
+    public jocMemory(Context c) {
+        // Required empty public constructor
+        context=c;
+    }
+
+    
+    public static jocMemory newInstance(String param1, String param2) {
+        jocMemory fragment = new jocMemory();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_joc_memory, container, false);
+        ImageView12 = (ImageView) view.findViewById(R.id.imageViewMemory12);
+        ImageView11 = (ImageView) view.findViewById(R.id.imageViewMemory11);
+        ImageView10 = (ImageView) view.findViewById(R.id.imageViewMemory10);
+        ImageView9 = (ImageView) view.findViewById(R.id.imageViewMemory9);
+        ImageView8 = (ImageView) view.findViewById(R.id.imageViewMemory8);
+        ImageView7 = (ImageView) view.findViewById(R.id.imageViewMemory7);
+        ImageView6 = (ImageView) view.findViewById(R.id.imageViewMemory6);
+        ImageView5 = (ImageView) view.findViewById(R.id.imageViewMemory5);
+        ImageView4 = (ImageView) view.findViewById(R.id.imageViewMemory4);
+        ImageView3 = (ImageView) view.findViewById(R.id.imageViewMemory3);
+        ImageView2 = (ImageView) view.findViewById(R.id.imageViewMemory2);
+        ImageView1 = (ImageView) view.findViewById(R.id.imageViewMemory1);
+
+        ImageView1.setOnClickListener(this);
+        ImageView2.setOnClickListener(this);
+        ImageView3.setOnClickListener(this);
+        ImageView4.setOnClickListener(this);
+        ImageView5.setOnClickListener(this);
+        ImageView6.setOnClickListener(this);
+        ImageView7.setOnClickListener(this);
+        ImageView8.setOnClickListener(this);
+        ImageView9.setOnClickListener(this);
+        ImageView10.setOnClickListener(this);
+        ImageView11.setOnClickListener(this);
+        ImageView12.setOnClickListener(this);
+        return view;
+    }
+    
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        CoolImageFlipper c = new CoolImageFlipper(context);
+        Drawable d=null;
+        jocMemory firstFragment;
+        switch (v.getId()){
+            case R.id.imageViewMemory1:
+                c.flipImage(d,ImageView1);
+                break;
+            case R.id.imageViewMemory2:
+                c.flipImage(d,ImageView2);
+                break;
+            case R.id.imageViewMemory3:
+                c.flipImage(d,ImageView3);
+                break;
+            case R.id.imageViewMemory4:
+                c.flipImage(d,ImageView4);
+                break;
+            case R.id.imageViewMemory5:
+                c.flipImage(d,ImageView5);
+                break;
+            case R.id.imageViewMemory6:
+                c.flipImage(d,ImageView6);
+                break;
+            case R.id.imageViewMemory7:
+                c.flipImage(d,ImageView7);
+                break;
+            case R.id.imageViewMemory8:
+                c.flipImage(d,ImageView8);
+                break;
+            case R.id.imageViewMemory9:
+                c.flipImage(d,ImageView9);
+                break;
+            case R.id.imageViewMemory10:
+                c.flipImage(d,ImageView10);
+                break;
+            case R.id.imageViewMemory11:
+                c.flipImage(d,ImageView11);
+                break;
+            case R.id.imageViewMemory12:
+                c.flipImage(d,ImageView12);
+                break;
+
+        }
+        //firstFragment = new jocMemory(context);
+        //getFragmentManager().beginTransaction().add(R.id.fragment_memory_container, firstFragment).commit();
+
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/arnauzapata/myapplication/memory.java
+++ app/src/main/java/com/arnauzapata/myapplication/memory.java
@@ -1,78 +1,22 @@
 package com.arnauzapata.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.FrameLayout;
 
 public class memory extends AppCompatActivity{
-    View fragment_0_0;View fragment_0_1;View fragment_0_2;
-    View fragment_1_0;View fragment_1_1;View fragment_1_2;
-    View fragment_2_0;View fragment_2_1;View fragment_2_2;
-    View fragment_3_0;View fragment_3_1;View fragment_3_2;
-    android.app.Fragment fragment;
 
-    private android.app.FragmentManager fragmentManager = getFragmentManager();
+
+    Context context=this;
     private String TAG = "Memory";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory);
-
-
-
-        FrameLayout frame = new FrameLayout(this);
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_0_0);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_0_0, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_0_1);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_0_1, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_0_2);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_0_2, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_1_0);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_1_0, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_1_1);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_1_1, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_1_2);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_1_2, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_2_0);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_2_0, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_2_1);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_2_1, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_2_2);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_2_2, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_3_0);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_3_0, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_3_1);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_3_1, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_3_2);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_3_2, fragment).commit();
-
-        fragment= new memoryCardUnchoosed(R.id.fragment_memory_3_2);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_3_2, fragment).commit();
-
-    }
-
-
-
-
-    public void unSelect(View v){
-
-    }
-
-    public void select(View v){
+        jocMemory firstFragment = new jocMemory(context);
+        //firstFragment.newInstance(context);
+        getFragmentManager().beginTransaction().add(R.id.fragment_memory_container, firstFragment).commit();
 
     }
 }
