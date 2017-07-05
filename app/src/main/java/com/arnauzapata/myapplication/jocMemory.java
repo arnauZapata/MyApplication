f026e8c5bb63a770ee3acbc20b4d68039232e230
arnauZapata <arnauzapata@hotmail.com>
2017-07-05 18:48:24 +0200

    Memory: iMPLEMENTANDO, solucion al memory en fase de pruebas

--- app/src/main/java/com/arnauzapata/myapplication/jocMemory.java
+++ app/src/main/java/com/arnauzapata/myapplication/jocMemory.java
@@ -31,16 +31,17 @@ public class jocMemory extends android.app.Fragment implements View.OnClickListe
     private ImageView ImageView11;
     private ImageView ImageView12;
 
-    int [] soluciones;
-    boolean [] solved;
-    boolean [] selected;
+    int[] soluciones =new int[12];
+    boolean [] solved=new boolean[12];
     int image1; int image2;
 
 
     Context context;
 
     public jocMemory() {
-        // Required empty public constructor
+        for(int i=0;i<12;i++) soluciones[i]=R.drawable.images;
+        for(int i=0;i<12;i++) solved[i]=false;
+        image1=-1;image2 = -1;
     }
     public jocMemory(Context c) {
         // Required empty public constructor
@@ -115,50 +116,99 @@ public class jocMemory extends android.app.Fragment implements View.OnClickListe
     @Override
     public void onClick(View v) {
         CoolImageFlipper c = new CoolImageFlipper(context);
-        Drawable d=null;
+        Drawable d;
         jocMemory firstFragment;
+        int auxImage;
         switch (v.getId()){
             case R.id.imageViewMemory1:
+                auxImage=soluciones[0];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView1);
                 break;
             case R.id.imageViewMemory2:
+                auxImage=soluciones[1];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView2);
                 break;
             case R.id.imageViewMemory3:
+                auxImage=soluciones[2];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView3);
                 break;
             case R.id.imageViewMemory4:
+                auxImage=soluciones[3];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView4);
                 break;
             case R.id.imageViewMemory5:
+                auxImage=soluciones[4];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView5);
                 break;
             case R.id.imageViewMemory6:
+                auxImage=soluciones[5];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView6);
                 break;
             case R.id.imageViewMemory7:
+                auxImage=soluciones[6];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView7);
                 break;
             case R.id.imageViewMemory8:
+                auxImage=soluciones[7];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView8);
                 break;
             case R.id.imageViewMemory9:
+                auxImage=soluciones[8];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView9);
                 break;
             case R.id.imageViewMemory10:
+                auxImage=soluciones[9];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView10);
                 break;
             case R.id.imageViewMemory11:
+                auxImage=soluciones[10];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView11);
                 break;
             case R.id.imageViewMemory12:
+                auxImage=soluciones[11];
+                if(image1==-1){}
+                else if(image2==-1){}
+                d = getResources().getDrawable(auxImage);
                 c.flipImage(d,ImageView12);
                 break;
 
         }
 
-        firstFragment = new jocMemory(context);
-        getFragmentManager().beginTransaction().add(R.id.fragment_memory_container, firstFragment).commit();
+        //firstFragment = new jocMemory(context);
+        //getFragmentManager().beginTransaction().add(R.id.fragment_memory_container, firstFragment).commit();
 
     }
 
